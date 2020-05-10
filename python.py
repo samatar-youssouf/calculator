9d193ae2fa5dde08a503e40648657b0a08d1c01f
Your Name <samatar_93@hotmail.co.uk>
2020-05-10 18:19:19 +0200

    calculator

---
+++ python.py
@@ -0,0 +1,99 @@
+#a = 1
+#b = 5
+
+#create if and else statment
+#print hello world if a is greater than b
+#add elif statment for the other way round
+
+#if a > b : 
+#    print("Hello world")
+#elif b > a :
+#    print("Hello world 2")
+
+#for x in range(101):
+#    print(x)
+
+#fruits = ['apple','cherry','pear']
+
+#for fruit in fruits:
+#    print(fruit)
+#    if fruit == 'cherry':
+#        break
+
+#for fruit in fruits:
+#    print(fruit)
+#    if fruit == 'cherry':
+#        continue
+"""
+for x in range(2 ,30, 3):
+    print(x)
+start at 2 finish at 30  go up by 3
+
+#print 1 - 100 if its even print even if its odd print odd
+for x in range (101):
+    if x%2 == 0:
+        print(x,' even')
+    else:
+        print(x,' odd')
+
++ - / * 
+
+% checks the if there is a remainder 
+
+def football() :
+
+    print('man u')
+    print('arsenal')
+    print('chelsea')
+
+
+a = 2
+b = 4
+c =3
+
+football()
+
+name = input(" what is your name? ")
+age = input(" what is your age?")
+print(name +  age)
+
+num1 = int(input("what is your first number ? "))
+operator = input(" + - * / ? ")
+num2 = int(input ("what is your second number ? "))
+
+if operator == "+":
+    answer = int(num1 + num2)
+    print(answer)
+elif operator == "-":
+    answer = int(num1 - num2)
+    print(answer)
+elif operator == "*":
+    answer = int(num1 * num2)
+    print(answer)
+elif operator == "/":
+    answer = int(num1 / num2)
+
+"""
+def calculator():
+
+    num1 = int(input("what is your first number ? "))
+    operator = input(" + - * / ? ")
+    num2 = int(input ("what is your second number ? "))
+
+    if operator == "+":
+        answer = int(num1 + num2)
+        print(answer)
+    elif operator == "-":
+        answer = int(num1 - num2)
+        print(answer)
+    elif operator == "*":
+        answer = int(num1 * num2)
+        print(answer)
+    elif operator == "/":
+        answer = int(num1 / num2)
+        print(answer)
+    
+calculator()
+
+
+
